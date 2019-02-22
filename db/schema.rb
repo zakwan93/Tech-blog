6fab12963a672cecb04bd22ac986a7ec3bf97413
zakwan93 <zakwan_bhaiyat@yahoo.com>
2019-02-22 11:53:26 -0500

    Create a migration and add a validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_02_22_010617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "deiscription"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
