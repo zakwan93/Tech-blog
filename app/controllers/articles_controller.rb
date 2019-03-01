2626c255722922ba9eb5cef8ed5622d37a15ab3b
zakwan93 <zakwan_bhaiyat@yahoo.com>
2019-02-28 21:36:33 -0500

    Create Authantication and design flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 	before_action :set_article, only: [:edit, :update, :show, :destroy]
+	before_action :require_user, except:[:index, :show]
+	before_action :require_same_user, only: [:edit, :update, :destroy]
 
 	def index	
 		@articles = Article.paginate(page: params[:page], per_page:5)
@@ -16,6 +18,7 @@ class ArticlesController < ApplicationController
 	def create
 		# render plain: params[:article].inspect
 		@article = Article.new(article_params)
+		@article.user = currunt_user
 		# @article.save
 		if @article.save 
 			flash[:notice] = "Article was sucessfully created"
@@ -52,5 +55,11 @@ class ArticlesController < ApplicationController
 		end
 		def article_params
 			params.require(:article).permit(:title, :deiscription)
+		end
+		def require_same_user
+			if currunt_user != @article.user
+				flash[:danger] = "You can only edit or delete your own article"
+				redirect_to root_path
+			end
 		end
 end
